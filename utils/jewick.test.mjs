f0c11dda7c8459006f0a00b49f67da5914510cc6
Khalil Abu-Dahab <khalil.abudahab@cgps.group>
2026-06-25 10:52:34 +0100

    Update jewick.test.mjs

--- utils/jewick.test.mjs
+++ utils/jewick.test.mjs
@@ -24,5 +24,3 @@ import jewickParser from "./jewick.mjs";
 const root = jewickParser(
   ["Bovine",0.69395,["Gibbon",0.36079,["Orangutan",0.33636,["Gorilla",0.17147,["Chimp",1.19268,"Human",0.11927],0.08386],0.06124],0.15057],0.54939,"Mouse",1.21460]
 );
-
-console.log(root);
